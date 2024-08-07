324f211c5f601c8d5243b56a13743876210d98a6
LeeJungBum <jungbum05114@gmail.com>
2024-08-07 14:23:06 +0900

    chore: 프로젝트 초기세팅

---
+++ src/test/java/com/wefood/front/FrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wefood.front;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
