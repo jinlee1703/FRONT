91247e45b2de322f4e94615930ce1c01ee1e9fb6
masiljangajji <xmfpdlsj0508@gmail.com>
2024-08-15 05:08:48 +0900

    feat: 회원가입 페이지 변경 및 Login,UserGet Response에 유저 Id추가

--- src/main/java/com/wefood/front/user/dto/response/UserGetResponse.java
+++ src/main/java/com/wefood/front/user/dto/response/UserGetResponse.java
@@ -1,5 +1,5 @@
 package com.wefood.front.user.dto.response;
 
-public record UserGetResponse(String phoneNumber, String password, String name, Boolean isSeller, Boolean isResign,
+public record UserGetResponse(Long id,String phoneNumber, String password, String name, Boolean isSeller, Boolean isResign,
                               String accountNumber) {
 }
