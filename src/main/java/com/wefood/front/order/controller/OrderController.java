3f9b0cda104ec92e68e8f2e008809e3df1e64f53
masiljangajji <xmfpdlsj0508@gmail.com>
2024-08-17 04:42:35 +0900

    feat: 주문디테일 html 이름변경 , 직거래일 포멧추가 #24

--- src/main/java/com/wefood/front/order/controller/OrderController.java
+++ src/main/java/com/wefood/front/order/controller/OrderController.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -27,7 +26,7 @@ public class OrderController {
     @GetMapping("/order-list")
     public String orderList(@CookieValue Long id, Model model) {
         model.addAttribute("orderList", orderAdaptor.findOrderList(id));
-        return "orderList";
+        return "order-list";
     }
 
     @GetMapping("/order")
