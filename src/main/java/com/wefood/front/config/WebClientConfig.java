89759f0bc548941754a5ea1d2e3d90606cff5b05
masiljangajji <xmfpdlsj0508@gmail.com>
2024-08-17 06:50:22 +0900

    feat: 로그인 유무를 확인하는 LoginInterceptor 추가 #24

--- src/main/java/com/wefood/front/config/WebClientConfig.java
+++ src/main/java/com/wefood/front/config/WebClientConfig.java
@@ -1,10 +1,12 @@
 package com.wefood.front.config;
 
+import com.wefood.front.global.interceptor.LoginInterceptor;
 import jakarta.servlet.annotation.WebServlet;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.time.Duration;
@@ -19,5 +21,13 @@ public class WebClientConfig implements WebMvcConfigurer {
                 .setReadTimeout(Duration.ofSeconds(3L))
                 .build();
     }
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                .addPathPatterns("/**"); // 모든 경로에 대해 인터셉터 적용
+    }
+
 
 }
---
+++ src/main/java/com/wefood/front/global/interceptor/LoginInterceptor.java
@@ -0,0 +1,31 @@
+package com.wefood.front.global.interceptor;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        // 컨트롤러에서 요청 처리 후, View 렌더링 전에 호출되는 메서드
+        if (modelAndView != null) {
+            Cookie[] cookies = request.getCookies();
+            boolean isLoggedIn = false;
+
+            if (cookies != null) {
+                for (Cookie cookie : cookies) {
+                    if ("id".equals(cookie.getName())) {
+                        isLoggedIn = true;
+                        break;
+                    }
+                }
+            }
+
+            modelAndView.addObject("login", isLoggedIn);
+        }
+    }
+
+}
